ff72e37ea461013f4ae99923d0a227da2b036b14
Mattes Wenzel <mattes@artfacts.net>
2019-01-23 00:11:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
+  componentWillMount() {
+    this.setState({ menuEnabled: false })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div
+          onClick={() =>
+            this.setState({ menuEnabled: !this.state.menuEnabled })
+          }
+          className="menu-button"
+        />
+        <div
+          className={
+            this.state.menuEnabled ? 'menu-panel enabled' : 'menu-panel'
+          }
+        >
+          <ul className="navigation">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/test">Test</a>
+            </li>
+          </ul>
+        </div>
+        <div className="App-content">
+          <Router>
+            <Switch>
+              <Route exact path="/" render={() => this.renderHome()} />
+              <Route exact path="/test" render={() => this.renderTest()} />
+            </Switch>
+          </Router>
+        </div>
       </div>
-    );
+    )
+  }
+
+  renderHome = () => {
+    return (
+      <div className="developer-card">
+        <h1 className="developer-name">Mattes Wenzel</h1>
+        <span className="developer-slogan">
+          Front-End Developer and musician
+        </span>
+      </div>
+    )
+  }
+
+  renderTest = () => {
+    return (
+      <div>
+        <p>This is a test</p>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
