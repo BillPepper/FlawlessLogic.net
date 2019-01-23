141b29d6a50baf5129c75fbfea00a76b5c7c9f53
Mattes Wenzel <mattes@artfacts.net>
2019-01-23 20:08:37 +0100

    add link to soundcloud on music page

--- src/App.js
+++ src/App.js
@@ -127,6 +127,9 @@ class App extends Component {
           allow="autoplay"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/307362001&color=%23626977&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         />
+        <a href="https://soundcloud.com/vimptools">
+          There is more where this comes from!
+        </a>
       </div>
     )
   }
